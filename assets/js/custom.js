c381acc2273fecb671618aa87710b092a295a0d6
Uriti Sai Abhishek <contact@uritisaiabhishek.online>
2021-04-24 20:01:07 +0530

    admin panel basic

---
+++ assets/js/custom.js
@@ -0,0 +1,189 @@
+// datatable
+
+$(document).ready(function () {
+	var table = $("table.display").DataTable({
+		lengthChange: true,
+		lengthMenu: [
+			[5, 10, 25, 50, -1],
+			[5, 10, 25, 50, "All"],
+		],
+		// buttons: [ 'copy','csv','print', 'excel', 'pdf', 'colvis' ],
+		searching: true,
+		language: {
+			searchPlaceholder: "Search",
+		},
+		paging: true,
+		order: [[0, "asc"]],
+		ordering: true,
+		info: true,
+		columnDefs: [
+			{
+				targets: [3],
+				orderable: false,
+			},
+		],
+		buttons: [
+			{ extend: "pdf", className: "btn-primary btn btn-sm" },
+			{ extend: "excel", className: "btn-success btn btn-sm" },
+			{ extend: "print", className: "btn-info btn btn-sm" },
+			{ extend: "csv", className: "btn-warning btn btn-sm" },
+			{ extend: "copy", className: "btn-danger btn btn-sm" },
+			{ extend: "colvis", className: "btn-secondary btn btn-sm" },
+		],
+		dom:
+			"<'row'<'col-md-3'l><'col-md-5'B><'col-md-4'f>>" +
+			"<'row'<'col-md-12'tr>>" +
+			"<'row'<'col-md-5'i><'col-md-7'p>>",
+	});
+
+	table.buttons().container().appendTo(".tabledisplay_wrapper .col-md-6:eq(0)");
+});
+// chart js
+var ctx = document.getElementById("barChart");
+var myChart = new Chart(ctx, {
+	type: "bar",
+	data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
+var ctx = document.getElementById("pieChart");
+var pieChart = new Chart(ctx, {
+	type: "pie",
+	data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
+var ctx = document.getElementById("donutChart");
+var pieChart = new Chart(ctx, {
+	type: "doughnut",
+	data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
+var ctx = document.getElementById("lineChart");
+var pieChart = new Chart(ctx, {
+	type: "line",
+	data: {
+		labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+		datasets: [
+			{
+				label: "# of Votes",
+				data: [12, 19, 3, 5, 2, 3],
+				backgroundColor: [
+					"rgba(255, 99, 132, 0.2)",
+					"rgba(54, 162, 235, 0.2)",
+					"rgba(255, 206, 86, 0.2)",
+					"rgba(75, 192, 192, 0.2)",
+					"rgba(153, 102, 255, 0.2)",
+					"rgba(255, 159, 64, 0.2)",
+				],
+				borderColor: [
+					"rgba(255, 99, 132, 1)",
+					"rgba(54, 162, 235, 1)",
+					"rgba(255, 206, 86, 1)",
+					"rgba(75, 192, 192, 1)",
+					"rgba(153, 102, 255, 1)",
+					"rgba(255, 159, 64, 1)",
+				],
+				borderWidth: 1,
+			},
+		],
+	},
+	options: {
+		scales: {
+			y: {
+				beginAtZero: true,
+			},
+		},
+	},
+});
